2a5127d9b3f6556c93bc4edbc29974db47879d1f
AugustRush <liupingwei30@gmail.com>
2017-01-11 17:35:45 +0800

    add MySQL

--- Package.swift
+++ Package.swift
@@ -24,5 +24,6 @@ let package = Package(
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+		.Package(url:"https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0)
     ]
 )
